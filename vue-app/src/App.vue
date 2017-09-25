c4e959fb273797af746e9f29420130ac6049c786
bergenhem <cbergenhem@gmail.com>
2017-09-25 15:21:36 -0400

    Add logic in Vue to prevent blank items

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -22,5 +22,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   margin-top: 60px;
+  margin-left: 20px;
 }
 </style>
--- vue-app/src/components/Hello.vue
+++ vue-app/src/components/Hello.vue
@@ -2,7 +2,7 @@
   <div>
     <h1>{{ title }}</h1>
     <input v-model="newItem" />
-    <button v-on:click="addItem">Add item</button>
+    <button type="button" v-on:click="addItem">Add item</button>
     <ul>
       <li v-for="item in listItems">
         {{ item.text }}
@@ -27,10 +27,12 @@ export default {
   },
   methods: {
     addItem() {
-      let newArray = this.listItems;
-      let newId = parseInt(newArray[newArray.length - 1].id, 10);
-      newArray.push({ id: newId + 1, text: this.newItem });
-      this.listItems = newArray;
+      if(this.newItem !== '') {
+        let newArray = this.listItems;
+        let newId = parseInt(newArray[newArray.length - 1].id, 10);
+        newArray.push({ id: newId + 1, text: this.newItem });
+        this.listItems = newArray;
+      }
     }
   }
 }
