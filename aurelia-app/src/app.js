b9abb87fab394c9ca19b4a9e23dc3bdfc1fab8a7
bergenhem <cbergenhem@gmail.com>
2017-09-25 15:14:02 -0400

    Add basic logic for Aurelia app

--- aurelia-app/src/app.js
+++ aurelia-app/src/app.js
@@ -4,9 +4,7 @@ export class App {
   configureRouter(config, router) {
     config.title = 'Aurelia';
     config.map([
-      { route: ['', 'welcome'], name: 'welcome',      moduleId: PLATFORM.moduleName('./welcome'),      nav: true, title: 'Welcome' },
-      { route: 'users',         name: 'users',        moduleId: PLATFORM.moduleName('./users'),        nav: true, title: 'Github Users' },
-      { route: 'child-router',  name: 'child-router', moduleId: PLATFORM.moduleName('./child-router'), nav: true, title: 'Child Router' }
+      { route: ['', 'items'], name: 'items', moduleId: PLATFORM.moduleName('./items'), nav: true, title: 'List Items' }
     ]);
 
     this.router = router;
