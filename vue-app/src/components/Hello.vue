6a7be9475481fa1fcf4ad362cc6a6cb96516800f
bergenhem <cbergenhem@gmail.com>
2017-09-25 11:41:44 -0400

    Add basic logic for Vue app

--- vue-app/src/components/Hello.vue
+++ vue-app/src/components/Hello.vue
@@ -1,21 +1,12 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+  <div>
+    <h1>{{ title }}</h1>
+    <input v-model="newItem" />
+    <button v-on:click="addItem">Add item</button>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+      <li v-for="item in listItems">
+        {{ item.text }}
+      </li>
     </ul>
   </div>
 </template>
@@ -25,7 +16,21 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      title: 'Vue.js App',
+      newItem: '',
+      listItems: [
+        { id: 0, text: 'Desks' },
+        { id: 1, text: 'Chairs' },
+        { id: 2, text: 'Coffee Cup' }
+      ]
+    }
+  },
+  methods: {
+    addItem() {
+      let newArray = this.listItems;
+      let newId = parseInt(newArray[newArray.length - 1].id, 10);
+      newArray.push({ id: newId + 1, text: this.newItem });
+      this.listItems = newArray;
     }
   }
 }
@@ -33,21 +38,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1 {
   font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
 }
 </style>
