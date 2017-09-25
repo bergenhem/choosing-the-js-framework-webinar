79c7c993df42b866a7b91b24f09c0a1c933f6054
bergenhem <cbergenhem@gmail.com>
2017-09-25 11:21:26 -0400

    Add basic logic to React app

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,21 +1,69 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+   super(props);
+   this.state = {
+     value: '',
+     listItems: [
+       { id: 0, text: 'Desks' },
+       { id: 1, text: 'Chairs' },
+       { id: 2, text: 'Coffee Cup' }
+     ]
+   };
+
+   //Syntax to make sure we know what "this" is in our two events
+   this.handleChange = this.handleChange.bind(this);
+   this.addItem = this.addItem.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  addItem() {
+    let newArray = this.state.listItems;
+    let newId = parseInt(newArray[newArray.length - 1].id, 10);
+    newArray.push({ id: newId + 1, text: this.state.value });
+    this.setState({ listItems: newArray });
+  }
   render() {
+    let renderedItems = this.state.listItems;
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <input value={ this.state.value } onChange= { this.handleChange }/>
+        <button onClick={ this.addItem }>Add Item</button>
+        <ul>
+          {
+            renderedItems.map((item) =>
+            {
+              return <li key={ item.id }>{ item.text }</li>
+            })
+          }
+        </ul>
       </div>
     );
   }
 }
 
 export default App;
+
+// title = 'Angular App';
+// listItems = ['Desks', 'Chairs', 'Coffee Cup'];
+// addItem(newItem: string) {
+//   if(newItem) {
+//     this.listItems.push(newItem);
+//   }
+// }
+//
+// <h1>{{ title }}</h1>
+//
+// <input #newItem />
+// <button (click)="addItem(newItem.value)">Add Item</button>
+//
+// <ul>
+//   <li *ngFor="let item of listItems">
+//     {{ item }}
+//   </li>
+// <ul>
